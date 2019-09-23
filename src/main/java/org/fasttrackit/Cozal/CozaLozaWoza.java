a21672eb9c6557c417feaf9695394104c019bd7a
Florea Daniel <floreadaniel542@yahoo.com>
2019-09-23 19:09:43 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/Cozal/CozaLozaWoza.java
@@ -0,0 +1,22 @@
+package org.fasttrackit.Cozal;
+
+public class CozaLozaWoza {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 110; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Coza");
+            } else if (i % 5 == 0) {
+                System.out.println("Loza");
+            } else if (i % 7 == 0) {
+                System.out.println("Woza");
+            } else {
+                System.out.println(i + " ");
+            }
+            if (i % 11 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
+

6e0c2c29bb1395b91dabac27b3e0e872362b4d6e
Florea Daniel <floreadaniel542@yahoo.com>
2019-09-23 19:12:30 +0300

    CozaLozaWoza

--- src/main/java/org/fasttrackit/Cozal/CozaLozaWoza.java
+++ src/main/java/org/fasttrackit/Cozal/CozaLozaWoza.java
@@ -5,11 +5,13 @@ public class CozaLozaWoza {
     public static void main(String[] args) {
         for (int i = 1; i <= 110; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("Coza");
+                System.out.println("CozaLoza ");
+            } else if (i % 3 == 0) {
+                System.out.println("Coza ");
             } else if (i % 5 == 0) {
-                System.out.println("Loza");
+                System.out.println("Loza ");
             } else if (i % 7 == 0) {
-                System.out.println("Woza");
+                System.out.println("Woza ");
             } else {
                 System.out.println(i + " ");
             }
